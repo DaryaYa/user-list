4b29529ed52beff72019812660945149166e4243
DaryaYa <lyalyaka@mail.ru>
2021-10-19 22:54:20 +0300

    feat: add search

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -4,6 +4,7 @@ import { Modal } from './Modal';
 import { fetchUsers, deleteItem } from "../store/action-creators/user";
 import { useTypedSelector } from "../hooks/useTypedSelector";
 import './userList.css';
+
 interface IUser {
   user: {};
 }
@@ -14,6 +15,7 @@ export const UserList: React.FC = () => {
   const dispatch = useDispatch();
   const [active, setActive] = useState<boolean>(false);
   const [user, setUser] = useState<IUser>({ user: {} });
+  const [search, setSearch] = useState<string>('');
 
   useEffect(() => {
     dispatch(fetchUsers())
@@ -32,32 +34,61 @@ export const UserList: React.FC = () => {
     setUser(el);
   }
 
+  const highlight = (str) => {
+    if (!search) return str
+    const regexp = new RegExp(search, 'ig')
+    const matchValue = str.match(regexp)
+    if (matchValue) {
+
+      return str.split(regexp).map((s, index, array) => {
+        if (index < array.length - 1) {
+          const c = matchValue.shift()
+          return <span key={index}>{s}<span className={'highlight'}>{c}</span></span>
+        }
+        return s
+      })
+    }
+    return str
+  }
+
   return (
     <div>
       <h1>List of Users</h1>
+      <div className="search">
+        <input type="text" placeholder="Search..." onChange={(e) => setSearch(e.target.value)} />
+      </div>
+
       <button className="fetch" onClick={() => dispatch(fetchUsers())}>FETCH USERS</button>
       <div className="list">
-        {users.map(el => {
-          const id = el.id;
-
-          return (
-            <div key={id} className="userList" >
-              <div className="button">
-                <button onClick={() => dispatch(deleteItem(id))}>X
-                  <span className="tooltiptext">Delete this User</span></button>
-              </div>
-              <div className="list-text" onClick={() => getActiveUser(el)}>
-                <div className="text" >
-                  <p>NAME: {el.name}</p>
-                  <p>USERNAME: {el.username}</p>
-                  <p>E-MAIL: {el.email}</p>
+        {users.filter(el => {
+          if (el.username.toLowerCase().includes(search.toLowerCase())) {
+            return el;
+          } if (el.name.toLowerCase().includes(search.toLowerCase())) {
+            return el;
+          } if (el.email.toLowerCase().includes(search.toLowerCase())) {
+            return el;
+          }
+        })
+          .map(el => {
+            const id = el.id;
+            return (
+              <div key={id} className="userList" >
+                <div className="button">
+                  <button onClick={() => dispatch(deleteItem(id))}>X
+                    <span className="tooltiptext">Delete this User</span></button>
                 </div>
-              </div>
+                <div className="list-text" onClick={() => getActiveUser(el)}>
+                  <div className="text" >
+                    <p>NAME: {highlight(el.name)}</p>
+                    <p>USERNAME: {highlight(el.username)}</p>
+                    <p>E-MAIL: {highlight(el.email)}</p>
+                  </div>
+                </div>
 
-            </div>
+              </div>
+            )
+          }
           )
-        }
-        )
         }
       </div>
 
